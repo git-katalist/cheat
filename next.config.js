c2f1c2d6b4c27c75f6165fa614844a8a46af2e0d
tsots <mosalatsotetsi@gmail.com>
2022-11-28 03:50:00 +0200

    first deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
+
+module.exports = nextConfig;
